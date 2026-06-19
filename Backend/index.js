1d9261ab0d5e2e939093e235ab166fd9911a2468
BimaSatria <gtbima01@gmail.com>
2026-06-19 14:21:15 +0700

    Perubahan Baru dari sisi Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import todoRoutes from "./routes/todos.js"; 
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("hello anjai");
-});
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/todos", todoRoutes);
 
 app.listen(5000, () => {
     console.log("server is listening on port 5000");
