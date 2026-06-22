b8e03584404f9b33e62da6e18825e25a1f9d73a3
BimaSatria <gtbima01@gmail.com>
2026-06-22 10:56:08 +0700

    Update backend bagian routes/todos.js

--- Backend/routes/todos.js
+++ Backend/routes/todos.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
         const newTodo = await pool.query(
             "INSERT INTO todo (description, completed) VALUES ($1, $2) RETURNING *",
             [description, completed || false]
-        );
+        ) ;
         res.json(newTodo.rows[0]);
     } catch (err) {
         console.error(err.message);
@@ -17,5 +17,45 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.get("/", async (req, res) =>{
+    try{
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { description, completed } =  req.body;
+        const updateTodo = await pool.query (
+            "UPDATE todo SET description = $1, completed = $2 WHERE todo_id = $3 RETURNING *",
+            [description, completed || false, id]
+        );
+        res.json({
+            message: "Todo was update",
+            todo   :updateTodo.rows[0],
+        });
+    }catch (err) {
+        console.error(err.message);
+        res.status(500).send("server Error");
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+        res.json({
+            message: "Todo Deleted sukses",
+        });
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 export default router;
